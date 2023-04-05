04b21461cd9e4c8fcd461d188ce3378d4eaaa868
HuoXu <2542610526@qq.com>
2023-04-05 23:59:06 +0800

    fix: 修复jdk的path

--- init/07-java.sh
+++ init/07-java.sh
@@ -14,7 +14,7 @@ if [ $? != 0 ]; then
 	sudo tar -C /usr/local -xzf jdk8.tar.gz
 	sudo chmod a+x /usr/local/jdk1.8.0_202/bin/java
 	echo 'export JAVA_HOME=/usr/local/jdk1.8.0_202' >> "${PROFILE_FILE}"
-	echo 'export PATH=$PATH:$JAVA_HOME' >> "${PROFILE_FILE}"
+	echo 'export PATH=$PATH:$JAVA_HOME/bin' >> "${PROFILE_FILE}"
 	source "${PROFILE_FILE}"
 	l_success "java $(java -version 2>&1 | sed '1!d' | sed -e 's/"//g' | awk '{print $3}') installed."
 	rm -f jdk8.tar.gz
