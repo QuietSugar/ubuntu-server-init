20178ce4f24926fadd4f766485f37ff829e78358
xu <sugarjellyfish@gmail.com>
2023-09-06 09:21:57 +0800

    修改GOPATH数据目录

--- init/04-golang.sh
+++ init/04-golang.sh
@@ -3,7 +3,8 @@
 . ./utils.sh
 
 PROFILE_FILE="${HOME}/.profile"
-
+GO_PATH_DIR="${HOME}/working/soft/go"
+exit 1
 source $PROFILE_FILE
 
 # Install golang
@@ -14,6 +15,7 @@ if [ $? != 0 ]; then
 	sudo tar -C /usr/local -xzf go.tar.gz
 	sudo chmod a+x /usr/local/go/bin/go
 	echo 'export PATH=$PATH:/usr/local/go/bin' >> "${PROFILE_FILE}"
+  echo "export GOPATH=${GO_PATH_DIR}" >> "${PROFILE_FILE}"
 	source "${PROFILE_FILE}"
 	l_success "golang $(go version) installed."
 	rm -f go.tar.gz
