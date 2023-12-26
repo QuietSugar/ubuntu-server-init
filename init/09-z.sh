056f0ba915b0076ad6785777a644b47a1f28f0ae
xu <sugarjellyfish@gmail.com>
2023-12-26 10:24:09 +0800

    update

--- init/09-z.sh
+++ init/09-z.sh
@@ -9,9 +9,9 @@ Z_SH_FILE="${Z_SH_DIR}/z.sh"
 
 if [ ! -s "${Z_SH_FILE}" ]; then
 	l_info "configuring z..."
-	http --download https://ghproxy.com/https://raw.githubusercontent.com/rupa/z/master/z.sh -o ${Z_SH_FILE}
+	http --download https://raw.githubusercontent.com/rupa/z/master/z.sh -o ${Z_SH_FILE}
   # 由zshrc管理
-  #	echo "source ${Z_SH_FILE}" >> "${ZSHRC_FILE}"
+  	echo "source ${Z_SH_FILE}" >> "${ZSHRC_FILE}"
 	l_success "z configured."
 else
 	l_skip "z already installed."
--- init/99-dotfiles.sh
+++ init/99-dotfiles.sh
@@ -13,10 +13,10 @@ else
 	l_info "configuring dotfiles..."
   # 使用rcm管理
   install_via_apt rcm
-  git clone --depth=1 https://ghproxy.com/https://github.com/romkatv/powerlevel10k.git ~/.zsh/powerlevel10k
-  git clone --depth=1 https://ghproxy.com/https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
-  git clone --depth=1 https://ghproxy.com/https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
-  git clone --depth=1 https://ghproxy.com/https://github.com/zsh-users/zsh-completions.git ~/.zsh/zsh-completions
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.zsh/powerlevel10k
+  git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+  git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
+  git clone --depth=1 https://github.com/zsh-users/zsh-completions.git ~/.zsh/zsh-completions
 	env RCRC=$DOTFILES_DIR/rcrc rcup -d $DOTFILES_DIR
 	l_success "dotfiles configured."
 fi
