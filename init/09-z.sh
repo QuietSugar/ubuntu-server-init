68f926e9bc40f3493ee860cd71d5d798a8ce0656
xu <sugarjellyfish@gmail.com>
2026-06-18 22:24:16 +0800

    fix: add Gitee fallback for z.sh download

--- init/09-z.sh
+++ init/09-z.sh
@@ -10,8 +10,15 @@ Z_SH_FILE="${Z_SH_DIR}/z.sh"
 
 if [ ! -s "${Z_SH_FILE}" ]; then
 	l_info "configuring z..."
-	fetch_github_raw "${Z_SH_FILE}" https://raw.githubusercontent.com/rupa/z/master/z.sh
-	l_success "z configured."
+	if ! fetch_github_raw "${Z_SH_FILE}" https://raw.githubusercontent.com/rupa/z/master/z.sh; then
+		l_warn "failed to download z from GitHub, trying Gitee mirror..."
+		if ! fetch "${Z_SH_FILE}" https://gitee.com/mirrors/rupa-z/raw/master/z.sh; then
+			l_warn "failed to download z, continuing without it"
+		fi
+	fi
+	if [ -s "${Z_SH_FILE}" ]; then
+		l_success "z configured."
+	fi
 else
 	l_skip "z already installed."
 fi
